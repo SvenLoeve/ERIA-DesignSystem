1484659e96b18c2eb09ccda39763dcfe3e910450
sloep <113307853+sloep@users.noreply.github.com>
2024-11-11 16:05:06 +0100

    fix theme toggle

--- src/layouts/includes/theme.js
+++ src/layouts/includes/theme.js
@@ -1,7 +1,29 @@
+let loaded;
 function setTheme(theme) {
 	document.documentElement.setAttribute('data-theme', theme);
 	localStorage.setItem('theme', theme);
+	if (loaded) {
+		setToggleActive(theme);
+	}
 }
+
+function setToggleActive(theme) {
+	const lightToggle = document.getElementById('light-theme-toggle');
+	const darkToggle = document.getElementById('dark-theme-toggle');
+
+	if (theme === 'light') {
+		lightToggle.classList.add('tab-active');
+		darkToggle.classList.remove('tab-active');
+	} else {
+		lightToggle.classList.remove('tab-active');
+		darkToggle.classList.add('tab-active');
+	}
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	setToggleActive(savedTheme || defaultTheme);
+	loaded = true;
+})
 
 const savedTheme = localStorage.getItem('theme');
 let defaultTheme = 'light';
